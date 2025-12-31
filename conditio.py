582314ccd77a77b99df738887ae0ebd0ae93a858
Mufeed Ahmad <mufeedahmad020@gmail.com>
2025-12-31 14:51:14 +0530

    Add files via upload

---
+++ conditio.py
@@ -0,0 +1,21 @@
+#Conditiolan statement
+
+
+# # If-else statement
+age = int(input("Enter your age :"))
+
+if(age>=18):
+    print("You can drive .")
+else:
+    print("you cannot drive")
+
+#elif statment
+a = int(input("Enter the  value of a :"))
+
+if (a <0):
+    print("Value is negetive")
+elif(a ==0):
+    print("Value is equal to zero")
+else:
+    print("Value is positive value ")
+    
